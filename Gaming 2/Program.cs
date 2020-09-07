244f8a32e665cb46eb014e1c16ca3cdc2994f7ab
arda <ardaburc.arslanazi@elev.ga.ntig.se>
2020-09-07 09:32:54 +0200

    commit

---
+++ Gaming 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Gaming_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv ditt namn");
+
+            String name = Console.ReadLine();
+
+
+            if (name == "lebron james" || name == "michael jordan")
+            {
+                Console.WriteLine("Välkommen, wakanda forever!");
+            }
+
+
+
+            // if (name != "micke")
+            //    Console.WriteLine(" lost, ya bum");
+            //}
+            else
+            {
+                Console.WriteLine("yu are lmao");
+            }
+
+
+            Console.ReadLine();
+
+            Jump();
+
+            Console.ReadLine();
+        }
+
+        static void Jump()
+        {
+            Console.WriteLine("it's large like ur lmao");
+
+            Jump();
+        }
+    }
+
+}
